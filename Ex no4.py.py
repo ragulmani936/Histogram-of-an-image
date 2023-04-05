01983b89308e5bdbd297a6cb6081c1eea6985c0a
RAGUL M <94881918+ragulmani936@users.noreply.github.com>
2023-04-05 20:03:42 +0530

    Add files via upload

---
+++ Ex no4.py.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import matplotlib.pyplot as plt
+
+
+# In[5]:
+
+
+gray_image = cv2.imread("cat.jpg")
+color_image = cv2.imread("house1.jpg",-1)
+cv2.imshow("Gray Image",gray_image)
+cv2.imshow("Colour Image",color_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[6]:
+
+
+gray_hist = cv2.calcHist([gray_image],[0],None,[256],[0,256])
+color_hist = cv2.calcHist([color_image],[0],None,[256],[0,256])
+plt.figure()
+plt.imshow(gray_image)
+plt.show()
+plt.title("Histogram")
+plt.xlabel("Grayscale Value")
+plt.ylabel("Pixel Count")
+plt.stem(gray_hist)
+plt.show()
+plt.imshow(color_image)
+plt.show()
+plt.title("Histogram of Color Image - Green Channel")
+plt.xlabel("Intensity Value")
+plt.ylabel("Pixel Count")
+plt.stem(color_hist)
+plt.show()
+
+
+# In[7]:
+
+
+gray_image = cv2.imread("cat.jpg",0)
+cv2.imshow("Gray Image",gray_image)
+equ = cv2.equalizeHist(gray_image)
+cv2.imshow("Equalized Image",equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+
+# In[ ]:
+
+
+
+
